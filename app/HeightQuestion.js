f7a9b4d75ab94539d9e18e1e02fdfef098060e83
Chris Greenwood <dr.greenwood@gmail.com>
2015-12-04 09:17:54 -0800

    finish height question functionality.

--- app/HeightQuestion.js
+++ app/HeightQuestion.js
@@ -6,18 +6,36 @@ var actions = require('./actions.js');
 
 var HeightQuestion = React.createClass({
   getInitialState: function () {
-    return {
-      value: this.props.value
-    };
+    var state = {
+      height: this.props.value,
+      feet: null,
+      inches: null
+    }
+
+    // ie. 6'10"
+    if(state.height && typeof state.height === 'string') {
+      console.log('Height value is '+state.height);
+      var matches = state.height.match(/(\d+)/g);
+      if(matches && matches.length == 2) {
+        state.feet = matches[0];
+        state.inches = matches[1];
+      }
+    }
+
+    return state;
   },
 
   handleSubmit: function(e) {
     e.preventDefault();
-    this.props.onAnswerQuestion(this.state.value);
+    var height = this.state.feet+"' "+this.state.inches+'"';
+    this.props.onAnswerQuestion(height);
   },
 
-  handleValueChange: function(e) {
-    this.setState({value: e.target.value});
+  handleFeetChange: function(e) {
+    this.setState({feet: e.target.value});
+  },
+  handleInchesChange: function(e) {
+    this.setState({inches: e.target.value});
   },
 
 	render: function() {
@@ -25,7 +43,8 @@ var HeightQuestion = React.createClass({
       <div className="question">
         <h3>{this.props.question.title}</h3>
         <form onSubmit={this.handleSubmit}>
-          <input name="number" onChange={this.handleValueChange} value={this.state.value} />
+          <input name="feet" onChange={this.handleFeetChange} value={this.state.feet} />
+          <input name="inches" onChange={this.handleInchesChange} value={this.state.inches} />
           <input type="submit" value="Next" />
         </form>
       </div>
